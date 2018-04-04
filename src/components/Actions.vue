bf9b67008fcbc41dc1e242178913b50fcdbfab0b
Owen Schebella <owenschebella@Owens-MacBook.local>
2018-04-04 23:35:26 +1000

    Centered actions content

--- src/components/Actions.vue
+++ src/components/Actions.vue
@@ -3,9 +3,39 @@
     <router-link v-bind:to="'/'">Home</router-link>
     <h1 class="mb-4">{{ msg }}</h1>
 
-    <div class="container mx-auto">
-      <div class="bg-#fafafa h-48 w-1/3 p-8 shadow">
-        <img src="/static/images/action1.svg">
+    <div class="pt-8 pb-8">
+    Visually differentiate between primary and secondary actions.
+      <div class="pt-4 mx-8 sm:flex sm:justify-center">
+        <div class="bg-#fafafa h-48 w-1/3 p-8 shadow">
+          Are you sure you want to continue with your primary action?
+          <div class="pt-8">
+            <img src="/static/images/action1.svg">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="pt-8 pb-8">
+    Visually differentiate between primary and secondary actions.
+      <div class="pt-4 mx-8 sm:flex sm:justify-center">
+        <div class="bg-#fafafa h-48 w-1/3 p-8 shadow">
+          Are you sure you want to continue with your primary action?
+          <div class="pt-8">
+            <img src="/static/images/action1.svg">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="pt-8 pb-8">
+    Visually differentiate between primary and secondary actions.
+      <div class="pt-4 mx-8 sm:flex sm:justify-center">
+        <div class="bg-#fafafa h-48 w-1/3 p-8 shadow">
+          Are you sure you want to continue with your primary action?
+          <div class="pt-8">
+            <img src="/static/images/action1.svg">
+          </div>
+        </div>
       </div>
     </div>
 
